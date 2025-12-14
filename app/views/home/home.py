084bd830cf7a1bd84f9f6774d8bfacfff44a73cf
Luiz Otávio <luiz.benedito@alunos.ifsuldeminas.edu.br>
2025-12-14 00:59:59 -0300

    feat: add HomeView to render home page template

---
+++ app/views/home/home.py
@@ -0,0 +1,6 @@
+from django.views import View
+from django.shortcuts import render
+
+class HomeView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'pages/home/home.html')
